ab67bd359941df4191c1fdd7a574139cceccf214
simen <simen@localhost>
2009-10-30 19:30:01 +0100

    Lol is gone

---
+++ src/game/game.h
@@ -0,0 +1,29 @@
+#ifndef GAME_H
+#define GAME_H
+
+// Logging functions
+#include "misc/log.h"
+
+// Forward declarations of modules
+class cmDraw;
+
+// Misc modules
+class cmTimer;
+class cmInput;
+
+class cmGame {
+    public:
+        int init(void);
+        void run(void);
+        
+        // Modules
+        class cmDraw *mDraw;
+
+        // Misc modules
+        class cmTimer *mTimer;
+        class cmInput *mIn;
+};
+
+extern class cmGame *mGame;
+
+#endif
