c16c9a322fcf4a80d39386fbd66d5bbf279ef055
simen <simen@localhost>
2009-10-31 13:05:22 +0100

    Better movement!

--- src/game/player/player.cpp
+++ src/game/player/player.cpp
@@ -2,15 +2,17 @@
 #include "player.h"
 #include "../draw.h"
 #include "../game.h"
-#include <stdio.h>
 
 void cmPlayer::init() {
     pos = cVector(200, 50);
-    gravity = 105;
+    vel = cVector(0, 0);
+    acc = cVector(0, 0);
+    gravity = 1.1;
     flagDown = false;
     flagUp = false;
     flagLeft = false;
     flagRight = false;
+    inAir = true;
 }
 
 void cmPlayer::input() {
@@ -40,28 +42,34 @@ void cmPlayer::process(double delta) {
     } else if (flagRight) {
         flagRight = false;
         vel = cVector(delta * HOR_SPEED, vel.y);
-    } else if (flagUp) {
-        flagUp = false;
-        vel = cVector(vel.x, 1);
     } else {
         vel = cVector(0, vel.y);
+    }
+
+    if (!inAir && flagUp) {
+        inAir = true;
+        acc = cVector(acc.x, -1 * JUMP_HEIGHT);
+        vel = cVector(vel.x, acc.y);
     }
 
     /* Temp. coll. detection, before we get some real ground */
     if (pos.y >= SCREEN_H - 100) {
         vel = cVector(vel.x, 0);
         pos.y = SCREEN_H - 100;
+        inAir = false;
     } else {
-        vel = cVector(vel.x, delta * (vel.y * gravity));
+        acc = cVector(acc.x, acc.y + gravity);
     }
+    
+    vel = cVector(vel.x + acc.x * delta, vel.y + acc.y * delta);
 
-    printf("%f\n", vel.y);
     pos += vel;
+    flagUp = false;
 }
 
 void cmPlayer::draw() {
-    rectfill(mGame->mDraw->buffer, pos.x, pos.y, pos.x + HOR_SPEED,
-            pos.y + HOR_SPEED, 200);
+    rectfill(mGame->mDraw->buffer, pos.x, pos.y, pos.x + 100,
+            pos.y + 100, 200);
 }
 
 void cmPlayer::clear_data() {
