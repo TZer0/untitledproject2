c5161436c08a6ed800e3e2779a77662574fa1d91
Martin Stensg?rd <ms7373@gmail.com>
2009-11-01 17:36:00 +0100

    Added comments to player.cpp.

--- src/game/player/player.cpp
+++ src/game/player/player.cpp
@@ -15,8 +15,10 @@ void cmPlayer::init() {
     inAir = true;
 }
 
+/*
+ * Set player flags based on keyboard input.
+ */
 void cmPlayer::input() {
-    
     if (key[KEY_LEFT]) {
         flagLeft = true;
     }
@@ -34,8 +36,13 @@ void cmPlayer::input() {
     }
 }
 
+/*
+ * Process player flags. Update player position.
+ *
+ * @param delta Delta
+ */
 void cmPlayer::process(double delta) {
-
+    // Update horizontal velocity and clear flags.
     if (flagLeft) {
         flagLeft = false;
         vel = cVector(delta * -HOR_SPEED, vel.y);
@@ -46,13 +53,14 @@ void cmPlayer::process(double delta) {
         vel = cVector(0, vel.y);
     }
 
+    // Jump handling.
     if (!inAir && flagUp) {
         inAir = true;
         acc = cVector(acc.x, -1 * JUMP_HEIGHT);
         vel = cVector(vel.x, acc.y);
     }
 
-    /* Temp. coll. detection, before we get some real ground */
+    // Temporary collision detection. Uses screen height instead of ground.
     if (pos.y >= SCREEN_H - 100) {
         vel = cVector(vel.x, 0);
         pos.y = SCREEN_H - 100;
@@ -61,15 +69,18 @@ void cmPlayer::process(double delta) {
         acc = cVector(acc.x, acc.y + gravity);
     }
     
+    // Update vertical velocity and clear flags.
     vel = cVector(vel.x + acc.x * delta, vel.y + acc.y * delta);
-
     pos += vel;
     flagUp = false;
 }
 
+/*
+ * Draw player sprite.
+ */
 void cmPlayer::draw() {
     rectfill(mGame->mDraw->buffer, pos.x, pos.y, pos.x + 100,
-            pos.y + 100, 200);
+            pos.y + 100, 100);
 }
 
 void cmPlayer::clear_data() {
