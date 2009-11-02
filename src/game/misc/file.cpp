c2155e28b7da016a2d3f7c08831edd9c49db4a24
Martin Stensg?rd <ms7373@gmail.com>
2009-11-02 22:57:30 +0100

    Proper indentation on file.cpp.

--- src/game/misc/file.cpp
+++ src/game/misc/file.cpp
@@ -1,7 +1,7 @@
 /**
  * File: file.cpp
  *
- * Description:	Miscellaneous functions to interact with files
+ * Description:    Miscellaneous functions to interact with files
  */
 // Global includes
 #include <math.h>
@@ -23,13 +23,13 @@ using namespace std;
  */
 double cmFile::read_double(PACKFILE *fp)
 {
-	int len;
-	char str[256];
-	
-	len = pack_getc(fp);
-	memset(str,0,len+1);
-	pack_fread(str,len,fp);
-	return(atof(str));
+    int len;
+    char str[256];
+    
+    len = pack_getc(fp);
+    memset(str,0,len+1);
+    pack_fread(str,len,fp);
+    return(atof(str));
 }
 
 /**
@@ -39,14 +39,14 @@ double cmFile::read_double(PACKFILE *fp)
  */
 char *cmFile::read_string(PACKFILE *fp)
 {
-	int len;
-	char *str;
-	
-	len = pack_igetl(fp);
-	str = new char[len+1];
-	memset(str,0,len+1);
-	pack_fread(str,len,fp);
-	return(str);
+    int len;
+    char *str;
+    
+    len = pack_igetl(fp);
+    str = new char[len+1];
+    memset(str,0,len+1);
+    pack_fread(str,len,fp);
+    return(str);
 }
 
 /**
@@ -54,33 +54,33 @@ char *cmFile::read_string(PACKFILE *fp)
  */
 void cmFile::set_data_directory(const char *dir)
 {
-	
-	// Give the data directory some memory
-	if(datadir != NULL) delete datadir;
-	datadir = new char[DATADIR_BUF_SIZE];
-	if(execdir != NULL) delete execdir;
-	execdir = new char[DATADIR_BUF_SIZE];
-	
-	memset(datadir,0,DATADIR_BUF_SIZE);
-	memset(execdir,0,DATADIR_BUF_SIZE);
-	
-	// We've first got to retrieve the executable directory
-	get_executable_name(execdir, DATADIR_BUF_SIZE);
-	
-	// Then we replace the directory with the datadir
-	replace_filename(datadir,execdir, dir, DATADIR_BUF_SIZE);
-	replace_filename(execdir,execdir, "", DATADIR_BUF_SIZE);
-	printf("%s\n",datadir);
-	
-	// Then we clean it up, just in case
-	if(datadir[strlen(datadir)-1]!='/') {
-		int len = strlen(datadir);
-		datadir[len] = '/';
-		datadir[len+1] = '\0';
-	}
-	
-	// And we're done \o/
-	LOGL(LDEBUG,"Data directory: %s", execdir_strip(datadir).c_str());
+    // Give the data directory some memory
+    if(datadir != NULL) delete datadir;
+    datadir = new char[DATADIR_BUF_SIZE];
+    if(execdir != NULL) delete execdir;
+    execdir = new char[DATADIR_BUF_SIZE];
+    
+    memset(datadir,0,DATADIR_BUF_SIZE);
+    memset(execdir,0,DATADIR_BUF_SIZE);
+    
+    // We've first got to retrieve the executable directory
+    get_executable_name(execdir, DATADIR_BUF_SIZE);
+    printf("%s\n",execdir);
+    
+    // Then we replace the directory with the datadir
+    replace_filename(datadir,execdir, dir, DATADIR_BUF_SIZE);
+    replace_filename(execdir,execdir, "", DATADIR_BUF_SIZE);
+    printf("%s\n",datadir);
+    
+    // Then we clean it up, just in case
+    if(datadir[strlen(datadir)-1]!='/') {
+        int len = strlen(datadir);
+        datadir[len] = '/';
+        datadir[len+1] = '\0';
+    }
+    
+    // And we're done \o/
+    LOGL(LDEBUG,"Data directory: %s", execdir_strip(datadir).c_str());
 }
 
 /**
@@ -92,66 +92,66 @@ void cmFile::set_data_directory(const char *dir)
  */
 std::list<string> cmFile::dirRecursiveGet(const char *tdir, const char *pat)
 {
-	std::list<string> dirlist;
-	std::list<string> filelist;
-	struct al_ffblk fd;	// Allegro structure for the file
-	string dirint; 		// Internal string
-	string curdir;		// Current directory
-	std::list<string>::iterator cur;
-	string dir;
-	
-	dirlist.clear(); filelist.clear();
-	
-	// Make sure there is no extra slash in the dir parameter
-	dir = string(tdir);
-	if(dir[dir.size()]=='/') dir.push_back('\0');
-	
-	// We will always search through the base directory
-	dirlist.push_back(string(datadir)+string(dir)+string("/"));
-	
-	// Recursively get all subdirectories
-	cur = dirlist.begin();
-	dirint = string(*cur)+string("*");
-	curdir = string(*cur)+string("/");
-	LOGS(LDEBUG,"Retrieving directories..");
-	while(cur != dirlist.end() && al_findfirst(dirint.c_str(), &fd, FA_ALL) == 0) {
-		LOGS(LDEBUG,"\tCurrent directory: %s", execdir_strip((*cur).c_str()).c_str());
-		do {
-			// Ignore unwanted directories
-			if(strcmp(fd.name,".")!=0 && strcmp(fd.name,"..")!=0) {
-				if((fd.attrib & FA_DIREC)) {
-					LOGS(LDEBUG,"\t\tDirectory: %s", fd.name);
-					dirlist.push_back(((*cur)+string(fd.name)+string("/")));
-				}
-			}
-		}while(al_findnext(&fd) == 0);
-		al_findclose(&fd);
-		cur++; // Go to next object in list
-		if(cur != dirlist.end()) {
-			dirint = string(*cur)+string("*");
-			curdir = string(*cur)+string("");
-		}
-	}
-	LOGS(LDEBUG,"Done retrieving directories");
-	
-	// From all directories, get filenames
-	LOGS(LDEBUG,"Retrieving filenames..");
-	for(cur=dirlist.begin();cur!=dirlist.end();cur++) {
-		LOGS(LDEBUG,"\tFrom directory %s", execdir_strip((*cur).c_str()).c_str());
-		// Get the first file in the directory, but go on if nothing is found (shouldn't happen)
-		if(al_findfirst(((*cur)+string("*")).c_str(),&fd, FA_ALL)!=0) continue;
-		
-		do {
-			if(!(fd.attrib & FA_DIREC) && strcmp(get_extension(fd.name),pat)==0) {
-				LOGS(LDEBUG,"\t\tFilename: %s", fd.name);
-				filelist.push_back((*cur)+string(fd.name));
-			}
-		}while(al_findnext(&fd)==0);
-		al_findclose(&fd);
-	}
-	LOGS(LDEBUG,"Done retrieving filenames");
-	
-	return filelist;
+    std::list<string> dirlist;
+    std::list<string> filelist;
+    struct al_ffblk fd;    // Allegro structure for the file
+    string dirint;         // Internal string
+    string curdir;        // Current directory
+    std::list<string>::iterator cur;
+    string dir;
+    
+    dirlist.clear(); filelist.clear();
+    
+    // Make sure there is no extra slash in the dir parameter
+    dir = string(tdir);
+    if(dir[dir.size()]=='/') dir.push_back('\0');
+    
+    // We will always search through the base directory
+    dirlist.push_back(string(datadir)+string(dir)+string("/"));
+    
+    // Recursively get all subdirectories
+    cur = dirlist.begin();
+    dirint = string(*cur)+string("*");
+    curdir = string(*cur)+string("/");
+    LOGS(LDEBUG,"Retrieving directories..");
+    while(cur != dirlist.end() && al_findfirst(dirint.c_str(), &fd, FA_ALL) == 0) {
+        LOGS(LDEBUG,"\tCurrent directory: %s", execdir_strip((*cur).c_str()).c_str());
+        do {
+            // Ignore unwanted directories
+            if(strcmp(fd.name,".")!=0 && strcmp(fd.name,"..")!=0) {
+                if((fd.attrib & FA_DIREC)) {
+                    LOGS(LDEBUG,"\t\tDirectory: %s", fd.name);
+                    dirlist.push_back(((*cur)+string(fd.name)+string("/")));
+                }
+            }
+        }while(al_findnext(&fd) == 0);
+        al_findclose(&fd);
+        cur++; // Go to next object in list
+        if(cur != dirlist.end()) {
+            dirint = string(*cur)+string("*");
+            curdir = string(*cur)+string("");
+        }
+    }
+    LOGS(LDEBUG,"Done retrieving directories");
+    
+    // From all directories, get filenames
+    LOGS(LDEBUG,"Retrieving filenames..");
+    for(cur=dirlist.begin();cur!=dirlist.end();cur++) {
+        LOGS(LDEBUG,"\tFrom directory %s", execdir_strip((*cur).c_str()).c_str());
+        // Get the first file in the directory, but go on if nothing is found (shouldn't happen)
+        if(al_findfirst(((*cur)+string("*")).c_str(),&fd, FA_ALL)!=0) continue;
+        
+        do {
+            if(!(fd.attrib & FA_DIREC) && strcmp(get_extension(fd.name),pat)==0) {
+                LOGS(LDEBUG,"\t\tFilename: %s", fd.name);
+                filelist.push_back((*cur)+string(fd.name));
+            }
+        }while(al_findnext(&fd)==0);
+        al_findclose(&fd);
+    }
+    LOGS(LDEBUG,"Done retrieving filenames");
+    
+    return filelist;
 }
 
 
@@ -161,22 +161,22 @@ std::list<string> cmFile::dirRecursiveGet(const char *tdir, const char *pat)
  */
 char *cmFile::get_script(const char *file)
 {
-	int len;
-	char *script;
-	PACKFILE *fp;
-	
-	// Load file into memory
-	len = file_size_ex(file);
-	script = new char[len+1];
-	memset(script, 0, sizeof(char)*(len+1));
-	
-	if((fp = pack_fopen(file, "r"))==NULL) {
-		LOGL(LERR, "Could not open file \'%s\' to get the script", file);
-	}
-	pack_fread(script, len, fp);
-	pack_fclose(fp);
-	
-	return script;
+    int len;
+    char *script;
+    PACKFILE *fp;
+    
+    // Load file into memory
+    len = file_size_ex(file);
+    script = new char[len+1];
+    memset(script, 0, sizeof(char)*(len+1));
+    
+    if((fp = pack_fopen(file, "r"))==NULL) {
+        LOGL(LERR, "Could not open file \'%s\' to get the script", file);
+    }
+    pack_fread(script, len, fp);
+    pack_fclose(fp);
+    
+    return script;
 }
 
 
@@ -186,17 +186,17 @@ char *cmFile::get_script(const char *file)
  */
 std::string cmFile::execdir_strip(const char *file)
 {
-	string tmp;
-	int i,len;
-	tmp = string(file);
-	i=0;
-	len = strlen(execdir);
-	while(i<len && tmp[i]==execdir[i]) {
-		i++;
-	}
-	tmp.erase(0,i);
-	
-	return tmp;
+    string tmp;
+    int i,len;
+    tmp = string(file);
+    i=0;
+    len = strlen(execdir);
+    while(i<len && tmp[i]==execdir[i]) {
+        i++;
+    }
+    tmp.erase(0,i);
+    
+    return tmp;
 }
 
 /**
@@ -205,5 +205,5 @@ std::string cmFile::execdir_strip(const char *file)
  */
 std::string cmFile::get_filename(const char *basedir, const char *filename)
 {
-	return(string(datadir)+string("/")+string(basedir)+string("/")+string(filename));
+    return(string(datadir)+string("/")+string(basedir)+string("/")+string(filename));
 }
