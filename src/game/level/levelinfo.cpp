242d40eb4fa775c6a92981e6e458c092a5dfeed6
stian <stian@localhost>
2009-11-12 23:28:42 +0100

    levelinfo.cpp

--- src/game/level/levelinfo.cpp
+++ src/game/level/levelinfo.cpp
@@ -7,18 +7,19 @@ string read_string(PACKFILE *fp);
 
 int cLevelInfo::load(PACKFILE *fp){
 
-    int type = read_int(fp);
+    while (true){
 
-    switch (type){
-        // Generates a switch
-        #define ATTR(name, file, type, load) case file: name = read_##load (fp); break;
-        #include "levelinfoattributes.h"
-        case 'NEXT': return 1;
-        case '_END': return 0;
-        default: break;
+        int type = read_int(fp);
+
+        switch (type){
+            // Generates a switch
+            #define ATTR(name, file, type, load) case file: name = read_##load (fp); break;
+            #include "levelinfoattributes.h"
+            case 'NEXT': return 1;
+            case '_END': return 0;
+            default: break;
+        }
     }
-
-    return 0;
 }
 
 int read_int(PACKFILE *fp){
