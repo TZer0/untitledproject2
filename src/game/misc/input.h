f55147fc1698d940bcaf2db9e2826450deefdd06
Martin Stensg?rd <ms7373@gmail.com>
2009-11-12 20:02:15 +0100

    Actual revert.

--- src/game/misc/input.h
+++ src/game/misc/input.h
@@ -2,22 +2,23 @@
  * File: input.h
  *
  * Description:	Header file for input.cpp
+ *
+ * (c)2009, by Raymond Loeberg
  */
 #ifndef INPUT_H
 #define INPUT_H
-#include <allegro5/allegro5.h>
+#include "../port.h"
 
 /// Stores pressed/released keys/mousebuttons and updates them on request
 class cmInput {
 	public:
-		void init();
 		void update(void);
 		
 		/// Is only true when a given key is hit 
-		char pkey[ALLEGRO_KEY_MAX];
+		char pkey[KEY_MAX];
 		
 		/// Is only true when a given key is released
-		char rkey[ALLEGRO_KEY_MAX];
+		char rkey[KEY_MAX];
 		
 		/// Is set when a mouse button is pressed
 		int mouse_pb;
@@ -25,23 +26,10 @@ class cmInput {
 		/// Is set when a mouse button is released
 		int mouse_rb;
 		
-		/// The relative position (compared to previous poll) of the scroll wheel
-		int m_scr;
-		
-		/// @name Portability stuff
-		//@{
-			char key[ALLEGRO_KEY_MAX];
-			int mouse_x,mouse_y;
-			int mouse_b;
-			int mouse_z;
-			
-			int keyrep;
-		//@}
 	private:
-		// Event queue
-		ALLEGRO_EVENT_QUEUE *events;
+		int mouse_old;
+		char key_old[KEY_MAX];
 };
 
-extern class cmInput mIn;
-
 #endif
+
