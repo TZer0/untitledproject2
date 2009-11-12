be1083a2ba881e8ce5e2136f2d4f998896671d78
tzer0.jan <tzer0.jan@localhost>
2009-11-12 22:00:13 +0100

    Starting on sounds.

---
+++ src/game/level/level.cpp
@@ -0,0 +1,46 @@
+#include "../port.h"
+#include "level.h"
+#include "../draw.h"
+#include "../game.h"
+
+void cmLevel::init() {
+    tiles.resize(6);
+    for (int j = 0; j<5; j++) {
+        tiles[j].resize(6);
+    }
+    tiles[0][0] = 1;
+    tiles[0][1] = 1;
+    tiles[1][0] = 1;
+    tiles[1][1] = 1;
+    tiles[2][0] = 1;
+    tiles[2][1] = 1;
+    tiles[3][0] = 1;
+    tiles[3][1] = 1;
+    tiles[4][0] = 1;
+    tiles[4][1] = 1;
+}
+
+void cmLevel::level_init() {
+}
+
+int cmLevel::load() {
+    return 0;
+}
+
+void cmLevel::process(double delta) {
+}
+
+void cmLevel::draw() {
+    int i, k;
+    for (k = 0; k<5; k++){
+        for (i = 0; i<5; i++){
+            if (tiles[k][i] == 1){
+            rectfill(mGame->mDraw->buffer, WTOS_X(k*32), WTOS_Y(i*32),WTOS_X(k*32)+ 32,
+                WTOS_Y(i*32+32), 0xff8800);
+            }
+        }
+    }
+}
+
+void cmLevel::clear_data() {
+}
---
+++ src/game/weapon/weapon.cpp
@@ -0,0 +1,25 @@
+#include "weapon.h"
+
+using namespace std;
+
+class cWeapon *cmWeapon::add(char *script, int ammo, bool ean) {
+    class cWeapon *newWeapon;
+    newWeapon = new cWeapon(script, ammo, ean);
+    weapons.push_back(newWeapon);
+    return newWeapon;
+}
+
+void cmWeapon::clear_data() {
+    // Delete cWeapon instances.
+}
+
+void cWeapon::fire(cVector pos, cVector vel) {
+    if (ammo != 0) {
+        luaL_dostring(l, script);
+        mGame->mBullet->add("print(1)\n", pos, vel);
+        if (ammo > 0)
+            --ammo;
+    } else {
+        LOGS(LDEBUG, "Out of ammo!");
+    }
+}
