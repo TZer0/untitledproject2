2dfbe4045baf230e2f50530c3ab83b8821a41a6b
stian <stian@localhost>
2009-11-12 22:10:55 +0100

    Loading level

--- src/game/level/levelinfoattributes.h
+++ src/game/level/levelinfoattributes.h
@@ -1,13 +1,15 @@
 // No header guards should be provided for this file
 // The file describes the attributes for the cLevelInfo class
-// ATTR(name, type) should be defined before including this file 
+// ATTR(name, file, type, load) should be defined before including this file 
 
 // FYI ATTR = Attribute
 
 #ifndef ATTR
-#error ATTR(name, type) macro should be defined
+#error ATTR(name, file, type, load) macro should be defined
 #endif
 
+// 'NEXT' and '_END' is reserved
+ATTR(name, 'NAME', std::string, string)
 ATTR(sizex, 'SIZX', int, int)
 ATTR(sizey, 'SIZY', int, int)
 ATTR(mapfile, 'MAPF', std::string, string)
