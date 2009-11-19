89779a693e8e1b79eebb3c9a2099b030c1234dca
tzer0.jan <tzer0.jan@localhost>
2009-11-19 19:28:13 +0100

    Tempfix for level

--- src/game/level/level.cpp
+++ src/game/level/level.cpp
@@ -10,11 +10,10 @@ void cmLevel::init() {
 
     // todo: put this call where it really belongs:
     load();
-
-    tiles.resize(LEVEL_HEIGHT);
-    for (int j = 0; j<LEVEL_HEIGHT; j++) {
-        tiles[j].resize(LEVEL_WIDTH);
-                tiles[j][21] = 1;
+    //tiles.resize(LEVEL_HEIGHT);
+    for (int j = 0; j<get_sizex(); j++) {
+        //tiles[j].resize(LEVEL_WIDTH);
+        tiles[j][18] = 1;
     }
     tiles[10][20] = 1;
     tiles[10][19] = 1;
@@ -23,8 +22,8 @@ void cmLevel::init() {
     rect = mGame->mCollision->create(CollisionRectangle, &colpos, cVector(0,0), 32, 32);
     
     // Register the tiles to the collision map
-    for (int k = 0; k<LEVEL_WIDTH; k++) {
-        for (int i = 0; i<LEVEL_HEIGHT; i++) {
+    for (int k = 0; k<get_sizex(); k++) {
+        for (int i = 0; i<get_sizey(); i++) {
             if(tiles[k][i] != 0) {
                 // Moves the collision position
                 colpos = cVector(k*32, i*32);
@@ -75,7 +74,7 @@ int cmLevel::load() {
     pack_fclose(pf);
 
     // resize level size:
-    tiles.resize(curLev->get_sizex());
+    tiles.resize(get_sizex());
 
     for (int x=0; x < get_sizex(); x++){
         tiles[x].resize(get_sizey());
@@ -107,8 +106,8 @@ void cmLevel::process(double delta) {
 
 void cmLevel::draw() {
     int i, k;
-    for (k = 0; k<LEVEL_WIDTH; k++){
-        for (i = 0; i<LEVEL_HEIGHT; i++){
+    for (k = 0; k<get_sizex(); k++){
+        for (i = 0; i<get_sizey(); i++){
             if (tiles[k][i] == 1){
             rectfill(mGame->mDraw->buffer, WTOS_X(k*32), WTOS_Y(i*32),WTOS_X(k*32+ 31),
                 WTOS_Y(i*32+31), 0xff8800);
