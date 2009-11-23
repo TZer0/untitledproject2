7a7e4c37163ed774d455fe686f9fe00e59db3487
avenger <avenger@phoenix>
2009-11-23 22:05:05 +0100

    Vector improvements

--- src/game/misc/vector.cpp
+++ src/game/misc/vector.cpp
@@ -2,6 +2,8 @@
  * File: vector.cpp
  *
  * Description:	Advanced vector class functions
+ *
+ * (c)2009, by Raymond Loeberg
  */
 // Global includes
 #include <math.h>
@@ -56,9 +58,15 @@ cVector cVector::rot(const cVector &b)
 	// Angle of this vector
 	double ang = atan2(-y,x)-M_PI/2.0;
 	
+	// Speed optimization:
+	// storing the cos and sin varaibles in temporary space.
+	// Deriving sin from cos, using sqrt(), which is faster
+	double cosv = cos(ang);
+	double sinv = sqrt(1.0-cosv*cosv);
+	
 	// Rotate the vector
-	tmp.x = cos(ang)*b.x-sin(ang)*b.y;
-	tmp.y = (sin(ang)*b.x+cos(ang)*b.y);
+	tmp.x = cosv*b.x-sinv*b.y;
+	tmp.y = sinv*b.x+cosv*b.y;
 	return(tmp);
 }
 
@@ -70,11 +78,15 @@ cVector cVector::unrot(const cVector &b)
 	cVector tmp;
 	
 	// Angle of this vector
-	double ang = -(atan2(-y,x)-M_PI/2.0);
+	double ang = atan2(-y,x)-M_PI/2.0;
+	
+	// See note for rot()
+	double cosv = cos(ang);
+	double sinv =-sqrt(1.0-cosv*cosv);
 	
 	// Rotate the vector
-	tmp.x = cos(ang)*b.x-sin(ang)*b.y;
-	tmp.y = (sin(ang)*b.x+cos(ang)*b.y);
+	tmp.x = cosv*b.x-sinv*b.y;
+	tmp.y = sinv*b.x+cosv*b.y;
 	return(tmp);
 }
 
