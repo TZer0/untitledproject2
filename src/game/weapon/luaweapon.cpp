469a09768d59f313f5d8857b1ce2a9612867ee6a
mastensg <mastensg@tott.ping.uio.no>
2009-12-10 22:49:35 +0100

    Lua weapon!

---
+++ src/game/weapon/luaweapon.cpp
@@ -0,0 +1,20 @@
+#include "luaweapon.h"
+#include "weapon.h"
+
+static int spawnBullet(lua_State *storS) {
+    cWeapon* cur = mGame->mWeapon->cur;
+    cur->spawnBullet(luaL_checkstring(storS, 1),
+                luaL_checknumber(storS, 2),
+                luaL_checknumber(storS, 3),
+                luaL_checknumber(storS, 4),
+                luaL_checknumber(storS, 5));
+    return 0;
+}
+
+static /*struct*/ luaL_Reg noe[] = {
+    {"bambam", spawnBullet},
+    NULL};
+
+void luaopen_weapon(lua_State *storS) {
+    luaL_register(storS, "_G", noe);
+}
