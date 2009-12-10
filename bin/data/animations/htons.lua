b579458a510bac549d17667982f0a4bd17d35113
TZer0 <TZer0.jan@gmail.com>
2009-12-10 21:16:18 +0100

    Map

---
+++ bin/data/animations/htons.lua
@@ -0,0 +1,13 @@
+new_animation("EULER")
+
+seq = {"EVIL"}
+
+sequence(seq[1], "euler.bmp", "")
+frame(0.1, 1, 1, 59, 64, 0, 0)
+new_animation("BUMBLE")
+
+seq = {"EVIL"}
+
+sequence(seq[1], "enemy.bmp", "")
+frame(0.1, 0, 0, 16, 9, 8, 5)
+
---
+++ src/game/level/level.h
@@ -0,0 +1,61 @@
+#ifndef LEVEL_H
+#define LEVEL_H
+
+#include "../misc/moduletemplate.h"
+#include "levelinfo.h"
+#include "tile.h"
+#include <map>
+#include <string>
+#include <vector>
+
+#include "../misc/moduletemplate.h"
+#include "levelinfo.h"
+#include "../collision/collision.h"
+#include "../collision/colapply.h"
+#include "../collision/colmap.h"
+
+#define MOD_LEVEL 42
+
+struct sLevelCollision {
+    // Short is short, saves us some memory.
+    // Only downside is it limits level size to 65535
+    unsigned short xi,yi;
+    cCollision *thisCollision;
+    sColSectorInstance cols;
+    sLevelCollision(cCollision *thisRect,unsigned short xi, unsigned short yi) : xi(xi), yi(yi), thisCollision(thisRect), cols(this)
+        { }
+};
+
+class cmLevel : public cDataSystem, public cApplyCollision {
+    private:
+        std::map <std::string, cLevelInfo*> levelInfo;
+        cLevelInfo *curLev; // current level
+        std::vector<std::vector <cTile> > tiles;
+        BITMAP *t[256];
+		BITMAP *background;
+        cCollision *rect;   // Rectangle collision instance
+        cVector colpos;     // Position of collision instance
+
+    public:
+        cmLevel() : cApplyCollision(MOD_LEVEL, 4, 1*32, 1*32)
+            { }
+        
+        void init();
+        void level_init();      
+        int load();
+        void process(double delta);
+        void draw();
+        void clear_data();
+
+        int get_sizex() {return curLev->get_sizex();}
+        int get_sizey() {return curLev->get_sizey();}
+
+        virtual ~cmLevel() {}
+        
+        // Collision functions
+        void collision_function(int caller_id, void*inst);
+        bool test_collision(int dial_id, cCollision *target) {return false;}
+};
+
+#endif
+
