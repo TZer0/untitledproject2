8acd6cf931163611837d3aaec089f0147f112538
stian <stian@localhost>
2009-12-18 23:33:52 +0100

    Cutting up delta, in hope that the player won't fall trough floor so often. Can the cause of lag be related to a high number of bullets on the screen at the same time?

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -15,6 +15,11 @@
 #include "bullet/bullet.h"
 #include "animation/anim.h"
 
+#define MAX_DELTA 0.1
+#ifndef min
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#endif
+
 void cmGame::run(void)
 {
     double delta;
@@ -29,12 +34,19 @@ void cmGame::run(void)
         
         if(mTimer->get_timer() > 0) {
             delta = mTimer->delta();
-            
+            double delta_left = delta;
+
             // Processing functions
             mPlayer->input();
-            mPlayer->process(delta);
-            mEnemy->process(delta);
-            mBullet->process(delta);
+
+            while (delta_left > 0){
+                double dist = min(delta_left, MAX_DELTA);
+                delta_left -= dist;
+
+                mPlayer->process(dist);
+                mEnemy->process(dist);
+                mBullet->process(dist);
+            }
             mAnim->process(delta);
 
             // Drawing functions
